9720d4f618a6239b6bbb998ad88a9290bb658fb6
Anton <info@ensostudio.ru>
2020-03-01 01:42:17 +0300

    Update userguide.php

--- config/userguide.php
+++ config/userguide.php
@@ -1,12 +1,11 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-return array(
-	'modules' => array(
-		'cli' => array(
-			'enabled' => TRUE,
-			'name' => 'CLI',
-			'description' => 'CLI is a simple command-line task runner.',
-			'copyright' => '&copy; 2009-2014 Kohana Team',
-		),
-	),
-);
+<?php
+return [
+	'modules' => ]
+		'cli' => [
+			'enabled'     => true,
+			'name'        => 'CLI',
+			'description' => 'Simple command-line task runner',
+			'copyright'   => '&copy; 2009-2020 WinterSilence',
+		],
+	],
+];
