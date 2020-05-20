81043dddda9141e7e8b4a03d718ca8f37c950bba
Anton <antonfedonjuk@gmail.com>
2020-05-20 21:47:46 +0300

    Update template.php

--- views/cli/help/scaffold/template.php
+++ views/cli/help/scaffold/template.php
@@ -1,29 +1,27 @@
-<?php defined('SYSPATH') OR die('No direct script access.') ?>
-<?php echo Kohana::FILE_SECURITY ?>
+<?='<?php';?>
 /**
- * CLI task class, uses automatic templating [View].
+ * CLI task with auto templating.
  *
- * @package   Kohana/CLI
- * @category  Task
- * @author    Kohana Team
- * @copyright (c) <?php echo date('Y') ?> Kohana Team
- * @license   http://kohanaframework.org/license
+ * @author    Kohana team
+ * @copyright (c) <?=date('Y');?> Kohana team
+ * @license   MIT
  */
-class <?php echo $kohana_cli_class ?> extends CLI_Task_Template {
+class <?=$kohana_cli_class;?> extends CLI_Task_Template
+{
 	/**
-	 * @var string|object Path to template or [View] object
+	 * @var string|object Path to template or `View` object
 	 */
-	protected $template = '<?php echo CLI_Tasker::class2path($kohana_cli_class) ?>';
+	protected $template = '<?=CLI_Tasker::class2path($kohana_cli_class);?>';
 
 	/**
-	 * @var boolean Auto render template
+	 * @var bool Auto render template
 	 **/
-	protected $auto_render = TRUE;
+	protected $auto_render = true;
 
 	/**
 	 * @var array Task options as array of option name => default values
 	 */
-	protected $options = array();
+	protected $options = [];
 
 	/**
 	 * Automatically executed after the task action.
@@ -32,7 +30,6 @@ class <?php echo $kohana_cli_class ?> extends CLI_Task_Template {
 	 */
 	protected function before()
 	{
-		// ...
 	}
 
 	/**
@@ -42,7 +39,6 @@ class <?php echo $kohana_cli_class ?> extends CLI_Task_Template {
 	 */
 	protected function action()
 	{
-		// ...
 	}
 
 	/**
@@ -52,6 +48,5 @@ class <?php echo $kohana_cli_class ?> extends CLI_Task_Template {
 	 */
 	protected function after()
 	{
-		// ...
 	}
 }
